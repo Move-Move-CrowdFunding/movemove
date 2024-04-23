7dd8fdfb625c81d5644d8c558b892caf43419a51
kenlee <larukuhyde05@gmail.com>
2024-04-23 23:53:15 +0800

    chore: 調整儲存自動格式化

--- app.vue
+++ app.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <h1 class=" flex text-3xl font-bold underline absolute">Hello world!</h1>
+    <h1 class="absolute flex text-3xl font-bold underline">Hello world!</h1>
     <NuxtWelcome />
   </div>
 </template>
 <script setup lang="ts">
-  const num: number = '123';
+const num: number = '123'
 </script>
 <style lang="scss">
 .box {
-  top: 50%;
   position: absolute;
-  padding: 0px;
+  top: 50%;
+  padding: 0;
 }
 </style>
