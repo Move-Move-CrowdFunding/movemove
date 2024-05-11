aca75a80987a94a98cd052c4f0c67fed88651452
kenlee <larukuhyde05@gmail.com>
2024-05-11 23:33:18 +0800

    feat: 新增提案列表資料、登出後導至首頁

--- composables/logout.ts
+++ composables/logout.ts
@@ -1,8 +1,9 @@
 const isLogin = useIsLoginStore()
 
-export const logout = () => {
+export const logout = async () => {
   const cookie = useCookie('userToken')
   cookie.value = null
   isLogin.isLogin = false
   alert('已登出')
+  await navigateTo('/')
 }
