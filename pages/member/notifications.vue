4d13ef493657a1380f22f47ecf7775e5574e2b48
kenlee <larukuhyde05@gmail.com>
2024-05-30 23:48:12 +0800

    feat: 串接最新通知 api

--- pages/member/notifications.vue
+++ pages/member/notifications.vue
@@ -1,55 +1,33 @@
 <script setup lang="ts">
 import type { ResponseData } from '~/types/response'
 
-// interface NotificationItem {
-//   id: string
-//   content: string
-//   createTime: number
-//   project: {
-//     id: string
-//     title: string
-//     coverUrl: string
-//   }
-// }
+interface NotificationItem {
+  id: string
+  content: string
+  createTime: number
+  project: {
+    id: string
+    title: string
+    coverUrl: string
+  }
+}
+
+const responsePagination = ref({
+  count: 0,
+  hasNext: false,
+  hasPre: false,
+  pageNo: 0,
+  pageSize: 0,
+  totalPage: 0
+})
+
 const { $dateformat } = useNuxtApp()
 
 const isLogin = useIsLoginStore()
 
 const tempUser = ref({})
 
-const notificationsList = ref([
-  {
-    id: '1',
-    content: '樂知修繕隊緊急求援|弱勢助弱勢,修家修心不能停',
-    createTime: 1728492012,
-    project: {
-      id: '1',
-      title: '樂知修繕隊緊急求援|弱勢助弱勢,修家修心不能停',
-      coverUrl:
-        'https://images.unsplash.com/photo-1599474924187-334a4ae5bd3c?q=80&w=1566&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    }
-  }
-  // {
-  //   id: 2,
-  //   title: '樂知修繕隊緊急求援|弱勢助弱勢,修家修心不能停',
-  //   createTime: 1728492012,
-  //   coverUrl:
-  //     'https://images.unsplash.com/photo-1599474924187-334a4ae5bd3c?q=80&w=1566&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  // },
-  // {
-  //   id: 3,
-  //   title: '樂知修繕隊緊急求援|弱勢助弱勢,修家修心不能停',
-  //   createTime: 1728492012,
-  //   coverUrl:
-  //     'https://images.unsplash.com/photo-1599474924187-334a4ae5bd3c?q=80&w=1566&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  // }
-])
-
-// 目前頁數
-const pageNo = ref(1)
-
-// 單頁筆數
-const pageSize = ref(1)
+const notificationsList: Ref<Partial<NotificationItem>[]> = ref([])
 
 const checkPermission = async () => {
   await isLogin.getUserData()
@@ -66,6 +44,12 @@ const getNotifications = async () => {
     method: 'GET'
   }).then((res) => {
     notificationsList.value = (res as ResponseData).results
+    responsePagination.value.count = (res as ResponseData).pagination.count
+    responsePagination.value.hasNext = (res as ResponseData).pagination.hasNext
+    responsePagination.value.hasPre = (res as ResponseData).pagination.hasPre
+    responsePagination.value.pageNo = (res as ResponseData).pagination.pageNo
+    responsePagination.value.pageSize = (res as ResponseData).pagination.pageSize
+    responsePagination.value.totalPage = (res as ResponseData).pagination.totalPage
   })
 }
 const getTempUser = (data: any) => {
@@ -82,13 +66,13 @@ onMounted(() => {
   <div class="py-10 lg:py-20">
     <div class="sm:px-auto container">
       <h1 class="mb-6 text-3xl sm:text-4xl lg:mb-10">最新通知</h1>
-      <ul class="flex flex-col">
+      <ul v-if="notificationsList && notificationsList.length > 0" class="flex flex-col">
         <li
           v-for="item in notificationsList"
           :key="item.id"
           class="relative top-0 flex-shrink-0 pb-6 [&:first-child>*>*:first-child]:top-[calc(50%_-_12px)] [&:last-child>*>*:first-child]:h-[calc(50%_-_12px)] [&:only-child>*>*:first-child]:hidden"
         >
-          <NuxtLink to="/">
+          <NuxtLink :to="`/projects/${item?.project?.id}`">
             <div class="absolute left-1 hidden h-full border-l-2 border-neutral-200 md:block"></div>
             <div
               class="absolute -left-[1px] -top-6 bottom-0 my-auto hidden h-3 w-3 items-center justify-center rounded-full bg-neutral-200 md:flex"
@@ -98,18 +82,18 @@ onMounted(() => {
             >
               <div class="relative h-56 w-full overflow-hidden lg:h-full">
                 <div
-                  :style="`background-image: url('${item.project.coverUrl}')`"
+                  :style="`background-image: url('${item?.project?.coverUrl}')`"
                   class="absolute inset-0 bg-cover bg-center duration-300 group-hover:scale-110"
                 ></div>
               </div>
               <div class="col-span-3 space-y-2 p-3 lg:px-6 lg:py-10">
                 <p class="text-neutral-600">
-                  {{ $dateformat(item.createTime) }}
+                  {{ item?.createTime ? $dateformat(item?.createTime) : '' }}
                 </p>
                 <h3 class="line-clamp-4 sm:text-[18px] md:line-clamp-2">
                   你發起的「<span
                     class="text-secondary-2 underline md:no-underline md:hover:underline"
-                    >{{ item.project.title }}</span
+                    >{{ item?.project?.title }}</span
                   >」已通過審核開始募資！
                 </h3>
               </div>
@@ -117,16 +101,19 @@ onMounted(() => {
           </NuxtLink>
         </li>
       </ul>
-      <div class="flex items-center justify-center pt-6">
+      <div
+        v-if="notificationsList && notificationsList.length > responsePagination.pageSize"
+        class="flex items-center justify-center pt-6"
+      >
         <UPagination
-          v-model="pageNo"
+          v-model="responsePagination.pageNo"
           :ui="{
             base: 'min-w-[44px] justify-center',
             rounded: 'rounded-md'
           }"
           size="xl"
           class="space-x-4"
-          :page-count="pageSize"
+          :page-count="responsePagination.pageSize"
           :total="notificationsList.length"
         />
       </div>
