b32c3ffbc4b2b42c48c6e1e5c2dee43ee489ec50
kenlee <larukuhyde05@gmail.com>
2024-06-02 23:02:50 +0800

    fix: 調整 ProjectInfo 顯示邏輯

--- components/ProjectInfo.vue
+++ components/ProjectInfo.vue
@@ -13,11 +13,17 @@ const props = defineProps({
 })
 
 const { tempData } = toRefs(props)
-
-// const maxCreateTimeObject = tempData?.state?.reduce((max, current) => {
-//   return current.createTime > max.createTime ? current : max
-// }, tempData.state[0])
-// tempData.value.status = maxCreateTimeObject?.status
+const newTempData = ref(tempData.value)
+// console.log(
+//   '!inAdmin && Array.isArray(newTempData.value.state)',
+//   !inAdmin && Array.isArray(newTempData.value.state)
+// )
+if (!inAdmin && Array.isArray(newTempData.value.state)) {
+  const maxCreateTimeObject = newTempData.value?.state?.reduce((max, current) => {
+    return current.createTime > max.createTime ? current : max
+  }, newTempData.value.state[0])
+  newTempData.value.status = maxCreateTimeObject?.status
+}
 
 // 10 天後
 const tenDaysLater = ref(dayjs(dayjs()).add(11, 'day').format('YYYY-MM-DD'))
@@ -27,27 +33,25 @@ const sevenDaysAfterTenDays = ref(dayjs(tenDaysLater.value).add(7, 'day').format
 
 // 綁定日期
 const dateInput = ref({
-  startDate: tenDaysLater.value || tempData.value.startDate,
-  endDate: sevenDaysAfterTenDays.value || tempData.value.endDate,
-  feedbackDate: tempData.value.feedbackDate || ''
+  startDate: tenDaysLater.value || newTempData.value.startDate,
+  endDate: sevenDaysAfterTenDays.value || newTempData.value.endDate,
+  feedbackDate: newTempData.value.feedbackDate || ''
 })
 
-tempData.value.startDate = dayjs(dateInput.value.startDate).unix()
-tempData.value.endDate = dayjs(dateInput.value.endDate).unix()
-tempData.value.feedbackDate = dayjs(dateInput.value.feedbackDate).unix() || 0
+newTempData.value.startDate = dayjs(dateInput.value.startDate).unix()
+newTempData.value.endDate = dayjs(dateInput.value.endDate).unix()
+newTempData.value.feedbackDate = dayjs(dateInput.value.feedbackDate).unix() || 0
 const changeDate = (item) => {
   const date = new Date(dateInput.value[item])
-  tempData.value[item] = date.getTime() / 1000
+  newTempData.value[item] = date.getTime() / 1000
 }
 
-const isDisable =
-  inAdmin || tempData.value?.state?.state === 0 || tempData.value?.state?.state === 1
+const isDisable = inAdmin || newTempData.value?.status === 0 || newTempData.value?.status === 1
 
 const coverUpload = ref(null)
 const feedbackUpload = ref(null)
 const uploadFile = async (item) => {
   const formData = new FormData()
-  formData.append('coverUpload', coverUpload.value.files[0])
   if (item === 'cover') {
     formData.append('coverUpload', coverUpload.value.files[0])
   } else {
@@ -63,9 +67,9 @@ const uploadFile = async (item) => {
   })
     .then((res) => {
       if (item === 'cover') {
-        tempData.value.coverUrl = res.results.imageUrl
+        newTempData.value.coverUrl = res.results.imageUrl
       } else {
-        tempData.value.feedbackUrl = res.results.imageUrl
+        newTempData.value.feedbackUrl = res.results.imageUrl
       }
     })
     .catch((err) => {
@@ -97,7 +101,7 @@ const reviewProjectId = (approve) => {
 <template>
   <div>
     <div class="container py-10">
-      <div v-if="tempData.value?.state?.state === -1" class="border-2 border-secondary-2">
+      <div v-if="newTempData?.status === -1" class="border-2 border-secondary-2">
         <div class="flex justify-between bg-secondary-2 p-3 font-bold text-white">
           <span>審核失敗</span>
           <span>2024/1/23</span>
@@ -114,7 +118,7 @@ const reviewProjectId = (approve) => {
             <label for="teamName">提案人姓名/團隊名稱</label>
             <input
               id="teamName"
-              v-model="tempData.teamName"
+              v-model="newTempData.teamName"
               type="text"
               placeholder="請輸入提案人姓名/團隊名稱"
               class="block w-full"
@@ -128,7 +132,7 @@ const reviewProjectId = (approve) => {
             <label for="email">聯絡信箱</label>
             <input
               id="email"
-              v-model="tempData.email"
+              v-model="newTempData.email"
               type="email"
               placeholder="請輸入聯絡信箱"
               class="block w-full"
@@ -139,7 +143,7 @@ const reviewProjectId = (approve) => {
             <label for="phone">聯絡手機</label>
             <input
               id="phone"
-              v-model="tempData.phone"
+              v-model="newTempData.phone"
               type="tel"
               placeholder="請輸入聯絡手機"
               class="block w-full"
@@ -150,7 +154,7 @@ const reviewProjectId = (approve) => {
             <label for="phone">團隊介紹</label>
             <textarea
               id="phone"
-              v-model="tempData.introduce"
+              v-model="newTempData.introduce"
               placeholder="請輸入團隊介紹"
               class="block w-full"
               :disabled="isDisable"
@@ -158,12 +162,12 @@ const reviewProjectId = (approve) => {
           </div>
           <div v-if="!inAdmin">
             <h2>專案卡預覽</h2>
-            <ProjectCard :project="tempData" />
+            <ProjectCard :project="newTempData" />
           </div>
           <div v-if="inAdmin">
             <h2>檢核紀錄</h2>
             <ul>
-              <li v-for="item in tempData.reviewLog" :key="item">
+              <li v-for="item in newTempData.reviewLog" :key="item">
                 {{ $timeformat(item.timestamp) }}
                 {{ item.state?.state === 0 ? '➖' : item.state?.state === -1 ? '✖️' : '✔️' }}
                 {{ item.content }}
@@ -177,7 +181,7 @@ const reviewProjectId = (approve) => {
             <label for="title">提案標題</label>
             <input
               id="title"
-              v-model="tempData.title"
+              v-model="newTempData.title"
               type="text"
               placeholder="請輸入提案標題"
               class="block w-full"
@@ -192,7 +196,7 @@ const reviewProjectId = (approve) => {
               <label for="categoryKey">分類</label>
               <select
                 id="categoryKey"
-                v-model="tempData.categoryKey"
+                v-model="newTempData.categoryKey"
                 name=""
                 class="block w-full"
                 :disabled="isDisable"
@@ -208,7 +212,7 @@ const reviewProjectId = (approve) => {
               <div class="flex items-center">
                 <input
                   id="targetMoney"
-                  v-model="tempData.targetMoney"
+                  v-model="newTempData.targetMoney"
                   type="text"
                   placeholder="請輸入提案目標"
                   class="block w-full"
@@ -248,7 +252,7 @@ const reviewProjectId = (approve) => {
             <label for="describe">提案簡介</label>
             <textarea
               id="describe"
-              v-model="tempData.describe"
+              v-model="newTempData.describe"
               placeholder="請簡短敘述提案內容, 最長不超過 80 字"
               class="block w-full"
               :disabled="isDisable"
@@ -273,20 +277,20 @@ const reviewProjectId = (approve) => {
                 @change="uploadFile('cover')"
               />
               <input
-                v-model="tempData.coverUrl"
+                v-model="newTempData.coverUrl"
                 type="text"
                 placeholder="或輸入圖片網址"
                 class="grow border-white"
                 :disabled="isDisable"
               />
             </div>
-            <img :src="tempData.coverUrl" class="mt-1" />
+            <img :src="newTempData.coverUrl" class="mt-1" />
           </div>
           <div class="mb-6">
             <label for="coverUrl">提案說明</label>
             <textarea
               id="coverUrl"
-              v-model="tempData.content"
+              v-model="newTempData.content"
               placeholder="請輸入提案說明, 至少 350 字"
               class="block w-full"
               :disabled="isDisable"
@@ -300,23 +304,23 @@ const reviewProjectId = (approve) => {
             <label for="videoUrl">影片網址</label>
             <input
               id="videoUrl"
-              v-model="tempData.videoUrl"
+              v-model="newTempData.videoUrl"
               type="text"
               placeholder="請輸入影片網址"
               class="block w-full"
               :disabled="isDisable"
             />
             <iframe
-              v-if="tempData.videoUrl"
+              v-if="newTempData.videoUrl"
               class="mt-2 aspect-video w-full"
-              :src="`https://www.youtube.com/embed/${tempData.videoUrl.split('&')[0].split('v=')[1]}`"
+              :src="`https://www.youtube.com/embed/${newTempData.videoUrl.split('&')[0].split('v=')[1]}`"
             ></iframe>
           </div>
           <div class="mb-6">
             <label for="relatedUrl">相關網站</label>
             <input
               id="relatedUrl"
-              v-model="tempData.relatedUrl"
+              v-model="newTempData.relatedUrl"
               type="text"
               placeholder="請輸入相關網站"
               class="block w-full"
@@ -329,7 +333,7 @@ const reviewProjectId = (approve) => {
             <label for="feedbackItem">回饋項目</label>
             <input
               id="feedbackItem"
-              v-model="tempData.feedbackItem"
+              v-model="newTempData.feedbackItem"
               type="text"
               placeholder="請輸入回饋項目"
               class="block w-full"
@@ -355,21 +359,21 @@ const reviewProjectId = (approve) => {
                 @change="uploadFile('feedback')"
               />
               <input
-                v-model="tempData.feedbackUrl"
+                v-model="newTempData.feedbackUrl"
                 type="text"
                 placeholder="或輸入圖片網址"
                 class="grow border-white"
                 :disabled="isDisable"
               />
             </div>
-            <img :src="tempData.feedbackUrl" class="mt-1" />
+            <img :src="newTempData.feedbackUrl" class="mt-1" />
           </div>
           <div class="mb-6">
             <label for="feedbackMoney">回饋門檻</label>
             <div class="flex items-center">
               <input
                 id="feedbackMoney"
-                v-model="tempData.feedbackMoney"
+                v-model="newTempData.feedbackMoney"
                 type="text"
                 placeholder="請輸入回饋門檻"
                 class="block w-full"
@@ -394,7 +398,7 @@ const reviewProjectId = (approve) => {
         </div>
       </div>
       <div
-        v-if="inAdmin && tempData?.state?.state == 0"
+        v-if="inAdmin && newTempData?.state?.state === 0"
         class="mt-10 flex flex-col gap-4 bg-secondary-5 px-3 py-10 sm:flex-row"
       >
         <input v-model="reviewContent" type="text" class="w-full" />
@@ -408,20 +412,20 @@ const reviewProjectId = (approve) => {
         </div>
       </div>
       <button
-        v-if="tempData.value?.state?.state === 0 && !inAdmin"
+        v-if="newTempData?.state === 0 && !inAdmin"
         class="mx-auto mt-10 block w-full rounded-lg bg-secondary-2 py-2 text-lg font-bold text-white hover:bg-primary-1 lg:w-96"
-        @click="emit('createProject', tempData)"
+        @click="emit('createProject', newTempData)"
       >
         發起提案
       </button>
       <button
-        v-if="tempData.value?.state?.state === 1 && !inAdmin"
+        v-if="newTempData?.status === 1 && !inAdmin"
         class="mx-auto mt-10 block w-full rounded-lg bg-warning-500 py-2 text-lg font-bold text-white hover:bg-warning-300 lg:w-96"
       >
         結束提案
       </button>
       <button
-        v-if="tempData.value?.state?.state === -1 && !inAdmin"
+        v-if="newTempData?.status === -1 && !inAdmin"
         class="mx-auto mt-10 block w-full rounded-lg bg-secondary-2 py-2 text-lg font-bold text-white hover:bg-primary-1 lg:w-96"
       >
         送出
