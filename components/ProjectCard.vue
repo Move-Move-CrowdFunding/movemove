abda733c297cf58c550ac2b58e2e8cacc3cd8a8b
yichienlu <yichien.lu89@gmail.com>
2024-06-04 23:05:48 +0800

    feat: create/success/[id]

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -13,7 +13,10 @@ const props = defineProps({
     }
   }
 })
-const { project } = props
+// const { project } = props
+const project = computed(() => {
+  return props.project
+})
 if (project._id) {
   project.id = project._id
 }
@@ -30,7 +33,7 @@ const toggleFollow = (id) => {
 </script>
 <template>
   <NuxtLink
-    :to="project.id ? `/projects/${project.id}` : null"
+    :to="project.id && project.state[0].status === 1 ? `/projects/${project.id}` : null"
     class="group block overflow-hidden rounded-3xl border border-primary-3 duration-300 hover:border-primary-1 hover:shadow-lg lg:rounded-[32px]"
   >
     <div class="relative overflow-hidden">
@@ -42,7 +45,7 @@ const toggleFollow = (id) => {
         v-if="isLogin.isLogin"
         class="group absolute right-1 top-1 z-10 flex h-10 w-10 items-center justify-center rounded-full bg-neutral-600/50 duration-300 hover:bg-secondary-1 active:fill-primary-1 lg:right-4 lg:top-4"
         :class="{ 'fill-primary-1': project.trackingStatus, 'fill-white': !project.trackingStatus }"
-        :disabled="!project.id"
+        :disabled="!project.id || project.state[0].status !== 1"
         @click.prevent="toggleFollow(project.id)"
       >
         <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -67,7 +70,7 @@ const toggleFollow = (id) => {
       </h3>
       <span
         class="inline-block rounded-full border border-primary-1 px-2 py-1 text-xs text-primary-1 sm:text-base"
-        >{{ categoryKeys[project.categoryKey - 1]?.name }}</span
+        >{{ categoryKeys[project.categoryKey - 1]?.name || '類別' }}</span
       >
       <div class="h-2 rounded-full bg-[#D9D9D9]">
         <div
@@ -80,7 +83,10 @@ const toggleFollow = (id) => {
         <p v-if="project.endDate < date.getTime() / 1000">已結束</p>
         <p v-else>
           倒數
-          <span>{{ Math.floor((project.endDate - date.getTime() / 1000) / 60 / 60 / 24) }}</span> 天
+          <span>{{
+            Math.floor((project.endDate - date.getTime() / 1000) / 60 / 60 / 24) || 0
+          }}</span>
+          天
         </p>
       </div>
     </div>
--- pages/create/success/[id].vue
+++ pages/create/success/[id].vue
@@ -1,59 +1,32 @@
 <script setup>
+import { useDayjs } from '#dayjs'
+const dayjs = useDayjs()
 const route = useRoute()
 
 const project = ref({
-  teamName: '弱勢救星',
-  email: 'movemove@gmail.com',
-  phone: '0912345678',
-  introduce: '專業金援團隊，弱勢族群救星，幫助許多需要協助的家庭。',
-  title: '愛心廚房｜溫飽無憂的一餐，舉辦食物援助計劃',
-  categoryKey: 2,
-  targetMoney: 300000,
-  startDate: 1700000000,
-  endDate: 1710000000,
-  describe: '一場無情的大火吞噬了整個社區，請幫助無家可歸的民眾。',
-  coverUrl:
-    'https://images.unsplash.com/photo-1711722221946-e271830d5081?q=80&w=2235&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  content:
-    '我們希望能幫助這些求助者，他們需要有個遮風避雨的住所，每月提供物資包與協助破損老舊的家，能得以維修,以減輕需要幫助的以及長輩們經濟開銷壓力。因此，發起修繕募資計畫，邀請社會大眾一同幫忙，協助清寒民眾與長輩房屋修繕，既使最初只能幫忙清理家園，讓長輩們有一個乾淨、舒適的生活環境，並定期提供長輩生活物資包，靠大家的力量，一同翻新清寒長輩與求助者們的生活。',
-  videoUrl: 'https://www.youtube.com/',
-  relateUrl: 'https://www.google.com.tw/',
-  feedbackItem: '限量精美小熊維尼 * 1',
-  feedbackUrl:
-    'https://plus.unsplash.com/premium_photo-1669632824466-09b2c595aa4c?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  feedbackMoney: '250',
-  feedbackDate: 1710000000,
-  status: {
-    status: 0,
-    content: ''
-  },
-  reviewLog: [
-    {
-      timestamp: 1700000000,
-      status: 0,
-      content: '提案送審'
-    },
-    {
-      timestamp: 1700000000,
-      status: -1,
-      content: '提案退回 - 請補上完整聯絡資訊'
-    },
-    {
-      timestamp: 1700000000,
-      status: 0,
-      content: '提案送審'
-    },
-    {
-      timestamp: 1700000000,
-      status: 1,
-      content: '提案通過'
-    }
-  ]
+  teamName: '',
+  email: '',
+  phone: '',
+  introduce: '',
+  title: '',
+  categoryKey: 0,
+  targetMoney: 0,
+  startDate: 0,
+  endDate: 0,
+  describe: '',
+  coverUrl: '',
+  content: '',
+  videoUrl: '',
+  relateUrl: '',
+  feedbackItem: '',
+  feedbackUrl: '',
+  feedbackMoney: 0,
+  feedbackDate: 0
 })
 const getProject = async () => {
   console.log('getProject')
   await getFetchData({
-    url: `/project/${route.params.id}`,
+    url: `/member/project/${route.params.id}`,
     method: 'GET'
   })
     .then((res) => {
@@ -72,10 +45,7 @@ onMounted(() => {
   <div>
     <CreateSteps :step="3" />
     <div class="container py-10">
-      665ee38359a03fd30bb27ae0
       <h1 class="mb-8 text-3xl font-bold">提案發起成功</h1>
-      <pre>{{ project }}</pre>
-      {{ route.params.id }}
       <div class="grid grid-cols-1 gap-10 sm:grid-cols-2 xl:grid-cols-3">
         <div class="xl:col-span-2">
           <ul>
@@ -90,9 +60,9 @@ onMounted(() => {
             <li class="mb-6">
               <div class="font-bold">提案發起日期：</div>
               <p>
-                <span v-dateformat="project.startDate"></span>
+                {{ dayjs(project.startDate * 1000).format('YYYY/MM/DD') }}
                 至
-                <span v-dateformat="project.endDate"></span>
+                {{ dayjs(project.endDate * 1000).format('YYYY/MM/DD') }}
               </p>
             </li>
             <li>
