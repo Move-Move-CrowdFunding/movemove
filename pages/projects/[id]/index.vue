719bd4541db268a3fadf507f374b5f5c2d62f793
yichienlu <yichien.lu89@gmail.com>
2024-06-06 17:05:23 +0800

    fix: date bug

--- pages/projects/[id]/index.vue
+++ pages/projects/[id]/index.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { useDayjs } from '#dayjs'
+const dayjs = useDayjs()
 const route = useRoute()
 const getProject = async () => {
   await getFetchData({
@@ -8,45 +10,44 @@ const getProject = async () => {
     .then((res) => {
       console.log(res)
       project.value = res.results
+      diffInSeconds.value = project.value.endDate - new Date() / 1000
+      days.value = Math.floor(diffInSeconds.value / (24 * 3600))
+      hours.value = Math.floor((diffInSeconds.value % (24 * 3600)) / 3600)
     })
     .catch((err) => console.log(err))
 }
 const project = ref({
-  id: '66058f16a069a14089afef0d',
-  userId: '354ae03a-9801-42ba-afa4-287b3b10df18',
-  projectId: '080a270d-7c2f-49ef-9a2c-bff1f5d33e51',
-  introduce: '專業金援團隊，弱勢族群救星，幫助許多需要協助的家庭。',
+  id: '',
+  userId: '',
+  projectId: '',
+  introduce: '',
   teamName: '弱勢救星',
-  title: '愛心廚房｜溫飽無憂的一餐，舉辦食物援助計劃',
-  email: 'movemove@gmail.com',
-  phone: '0912345678',
-  achievedMoney: 36020,
-  targetMoney: 50000,
-  categoryKey: 1,
-  content:
-    '<p>我們希望能幫助這些求助者，他們需要有個遮風避雨的住所，每月提供物資包與協助破損老舊的家，能得以維修,以減輕需要幫助的以及長輩們經濟開銷壓力。因此，發起修繕募資計畫，邀請社會大眾一同幫忙，協助清寒民眾與長輩房屋修繕，既使最初只能幫忙清理家園，讓長輩們有一個乾淨、舒適的生活環境，並定期提供長輩生活物資包，靠大家的力量，一同翻新清寒長輩與求助者們的生活。</p>',
-  coverUrl:
-    'https://images.unsplash.com/photo-1711722221946-e271830d5081?q=80&w=2235&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  describe: '一場無情的大火吞噬了整個社區，請幫助無家可歸的民眾。',
-  videoUrl: 'https://www.youtube.com/watch?v=YkVjY1F-Eoc',
-  startDate: 1712016024,
-  endDate: 1718016034,
-  createTime: 1711929600,
-  updateTime: 1712016000,
-  relatedUrl: 'https://www.google.com.tw/',
-  feedbackItem: '限量精美小熊維尼 * 1',
-  feedbackUrl:
-    'https://plus.unsplash.com/premium_photo-1669632824466-09b2c595aa4c?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  feedbackMoney: 250,
-  feedbackDate: 1712016000,
-  supportCount: 24,
-  trackingStatus: true
+  title: '',
+  email: '',
+  phone: '',
+  achievedMoney: 0,
+  targetMoney: 0,
+  categoryKey: 0,
+  content: '',
+  coverUrl: '',
+  describe: '',
+  videoUrl: '',
+  startDate: 0,
+  endDate: 0,
+  createTime: 0,
+  updateTime: 0,
+  relatedUrl: '',
+  feedbackItem: '',
+  feedbackUrl: '',
+  feedbackMoney: 0,
+  feedbackDate: 0,
+  supportCount: 0,
+  trackingStatus: false
 })
 
-const diffInSeconds = project.value.endDate - new Date() / 1000
-const days = Math.floor(diffInSeconds / (24 * 3600))
-const hours = Math.floor((diffInSeconds % (24 * 3600)) / 3600)
-const minutes = Math.floor((diffInSeconds % 3600) / 60)
+const diffInSeconds = ref(0)
+const days = ref(0)
+const hours = ref(0)
 onMounted(() => {
   nextTick(async () => {
     await getProject()
@@ -64,14 +65,14 @@ onMounted(() => {
         <div class="space-y-4">
           <div>
             <span class="rounded-full border border-primary-1 px-2 py-1 text-primary-1">{{
-              categoryKeys[project.categoryKey].name
+              categoryKeys[project.categoryKey - 1]?.name || '類別'
             }}</span>
           </div>
           <h1 class="text-3xl font-bold lg:text-4xl">{{ project.title }}</h1>
           <p>{{ project.describe }}</p>
           <div class="text-lg">
-            募資期間 <span v-dateformat="project.startDate"></span> ~
-            <span v-dateformat="project.endDate"></span>
+            募資期間 <span>{{ dayjs(project.startDate * 1000).format('YYYY/MM/DD') }}</span> ~
+            <span>{{ dayjs(project.endDate * 1000).format('YYYY/MM/DD') }}</span>
           </div>
           <div class="space-y-2">
             <div>目標 {{ priceFormat(project.targetMoney) }}</div>
@@ -87,7 +88,7 @@ onMounted(() => {
             </div>
             <div class="flex justify-between">
               <clientOnly>
-                <div>募資倒數 {{ days }} 天 {{ hours }} 小時 {{ minutes }} 分鐘</div>
+                <div>募資倒數 {{ days }} 天 {{ hours }} 小時</div>
               </clientOnly>
               <div class="flex items-center">
                 <Icon name="mdi:user" width="24" height="24" />
