aec8d5436c3d561c73deb084963fe52655fec4a0
kenlee <larukuhyde05@gmail.com>
2024-06-09 20:24:39 +0800

    fix: 修正圖片尺寸

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -8,10 +8,6 @@ defineProps({
     type: String,
     default: ''
   },
-  defaultImageSize: {
-    type: String,
-    default: 'lg'
-  },
   imageSize: {
     type: String,
     default: '40px'
@@ -19,14 +15,15 @@ defineProps({
 })
 </script>
 <template>
-  <div class="relative flex-shrink-0" :class="`h-[${imageSize}] w-[${imageSize}]`">
+  <div class="relative flex-shrink-0 overflow-hidden" :class="`h-[${imageSize}] w-[${imageSize}]`">
     <div
       v-if="!src"
       class="m-auto flex h-full w-full flex-shrink-0 items-center justify-center overflow-hidden rounded-full border border-neutral-200 bg-neutral-200 p-2 before:block before:h-full before:w-full before:bg-neutral-50 before:content-[''] before:[mask-image:url('~/assets/icons/avatar.svg')] before:[mask-position:center] before:[mask-repeat:no-repeat] before:[mask-size:contain]"
     ></div>
     <img
       v-else
-      class="h-full w-full overflow-hidden rounded-full border border-neutral-200 object-cover"
+      class="overflow-hidden rounded-full border border-neutral-200 object-cover"
+      :class="`h-[${imageSize}] w-[${imageSize}]`"
       :alt="alt"
       :src="src"
     />
