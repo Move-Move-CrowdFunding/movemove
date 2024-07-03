9bd09eec1efbfc82b7796f0562e53f571c551636
yichienlu <yichien.lu89@gmail.com>
2024-07-04 00:14:15 +0800

    feat: notification API & following checkPermission

--- pages/member/notifications.vue
+++ pages/member/notifications.vue
@@ -50,7 +50,6 @@ const getNotifications = async () => {
     url: `/member/notification?pageNo=${pageNo.value}&pageSize=${pageSize.value}`,
     method: 'GET'
   }).then((res) => {
-    console.log(res)
     notificationsList.value = (res as ResponseData).results
     responsePagination.value.count = (res as ResponseData).pagination.count
     responsePagination.value.hasNext = (res as ResponseData).pagination.hasNext
@@ -96,14 +95,16 @@ watch(
           :key="item.id"
           class="relative top-0 flex-shrink-0 pb-6 [&:first-child>*>*:first-child]:top-[calc(50%_-_12px)] [&:last-child>*>*:first-child]:h-[calc(50%_-_12px)] [&:only-child>*>*:first-child]:hidden"
         >
-          <NuxtLink :to="`/projects/${item?.project?.id}`">
+          <NuxtLink
+            :to="`/${item.project.userId === tempUser.id ? 'member/my-' : ''}projects/${item?.project?.id}`"
+          >
             <div class="absolute left-1 hidden h-full border-l-2 border-neutral-200 md:block"></div>
             <div
               class="absolute -left-[1px] -top-6 bottom-0 my-auto hidden h-3 w-3 items-center justify-center rounded-full bg-neutral-200 md:flex"
               :class="{ 'bg-primary-1': !item.isRead }"
             ></div>
             <div
-              class="group -top-[3px] ml-0 grid grid-cols-1 items-center overflow-hidden rounded-3xl border border-primary-3 duration-300 hover:border-primary-1 hover:shadow md:ml-7 lg:grid-cols-4"
+              class="group -top-[3px] ml-0 grid grid-cols-1 items-center overflow-hidden rounded-3xl border border-primary-3 duration-300 hover:border-primary-1 hover:shadow-lg md:ml-7 lg:grid-cols-4"
             >
               <div class="relative h-56 w-full overflow-hidden lg:h-full">
                 <div
